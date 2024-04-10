d2ef9e710100aa6f0cbacdf7c95cfb3abfb4d4ce
jerrodmpackard <jerrodmpackard@gmail.com>
2024-04-09 23:06:19 -0700

    updated bathroom model to add latitude and longitude properties

--- Models/BathroomModel.cs
+++ Models/BathroomModel.cs
@@ -7,8 +7,12 @@ namespace pottymapbackend.Models
 {
     public class BathroomModel
     {
+        // Leaving these string properties as nullable allows them to accept a null value, but we can still require the user to enter a value for each of them on the front-end
+
         public int ID { get; set; }
         public string? Location { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
         public string? Gender { get; set; }
         public string? Type { get; set; }
         public string? NumberOfStalls { get; set; }
