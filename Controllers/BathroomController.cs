9cf0da5f19bff29e8e3d388505b56afe879d3946
jerrodmpackard <jerrodmpackard@gmail.com>
2024-04-10 14:24:04 -0700

    added endpoints for adding, getting, updating, and deleting bathrooms

--- Controllers/BathroomController.cs
+++ Controllers/BathroomController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using pottymapbackend.Models;
+using pottymapbackend.Services;
 
 namespace pottymapbackend.Controllers
 {
@@ -10,6 +12,44 @@ namespace pottymapbackend.Controllers
     [Route("[controller]")]
     public class BathroomController : ControllerBase
     {
-        
+        private readonly BathroomService _data;
+
+        public BathroomController(BathroomService data)
+        {
+            _data = data;
+        }
+
+        // Add bathroom
+        [HttpPost]
+        [Route("AddBathroom")]
+        public bool AddBathroom(BathroomModel newBathroom)
+        {
+            return _data.AddBathroom(newBathroom);
+        }
+
+        // Get bathrooms
+        [HttpGet]
+        [Route("GetAllBathrooms")]
+        public IEnumerable<BathroomModel> GetAllBathrooms()
+        {
+            return _data.GetAllBathrooms();
+        }
+
+        // Update bathroom
+        // Since we are updating a bathroom, we want to take in the entire BathroomModel and call it bathroomUpdate
+        [HttpGet]
+        [Route("UpdateBathroom")]
+        public bool UpdateBathroom(BathroomModel bathroomUpdate)
+        {
+            return _data.UpdateBathroom(bathroomUpdate);
+        }
+
+        // Delete bathroom
+        [HttpDelete]
+        [Route("DeleteBathroom")]
+        public bool DeleteBathroom(BathroomModel bathroomToDelete)
+        {
+            return _data.DeleteBathroom(bathroomToDelete);
+        }
     }
 }
--- Services/BathroomService.cs
+++ Services/BathroomService.cs
@@ -2,11 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using pottymapbackend.Models;
+using pottymapbackend.Services.Context;
 
 namespace pottymapbackend.Services
 {
     public class BathroomService
     {
-        
+        private readonly DataContext _context;
+
+        public BathroomService(DataContext context)
+        {
+            _context = context;
+        }
+        public bool AddBathroom(BathroomModel newBathroom)
+        {
+            _context.Add(newBathroom);
+            // Any return other than zero, we save changes. This is becasue this function is set up as a bool
+            return _context.SaveChanges() != 0;
+        }
+
+        public IEnumerable<BathroomModel> GetAllBathrooms()
+        {
+            return _context.BathroomInfo;
+        }
+
+        public bool UpdateBathroom(BathroomModel bathroomUpdate)
+        {
+            _context.Update<BathroomModel>(bathroomUpdate);
+            return _context.SaveChanges() != 0;
+        }
+
+        public bool DeleteBathroom(BathroomModel bathroomToDelete)
+        {
+            bathroomToDelete.IsDeleted = true;
+            _context.Update<BathroomModel>(bathroomToDelete);
+            return _context.SaveChanges() != 0;
+        }
     }
 }
